239a994671ded68023b1f44d0520bca2f3c63bf4
pgeethika <55523801+pgeethika@users.noreply.github.com>
2020-08-22 15:51:15 +0530

    Add files via upload

--- checkout.js
+++ checkout.js
@@ -6,17 +6,17 @@ const mongodb =  require('mongodb')
 const router = express.Router()
 
 
+
 //connect db
 const connection_string = encodeURI('mongodb+srv://illenium_backend:Sc97s820skQKJWQb@illenium.lyr07.gcp.mongodb.net/illenium?retryWrites=true&w=majority')
 
-//load Userdetails details
-const userdetails = require('../details/userdetails');
 
 //Get method
 
 router.get('/',(req,res) => res.send("checkout"))
 
-// get the product details
+
+
 router.get('/',(req ,res)=>{
 
     let order = req.query.order
@@ -38,9 +38,44 @@ res.send('Product not found')
 }
 })
 
+
+
 //Post method
 
-router.post('/update/:order_ID',  async (req, res) => {
+
+router.post('/productdetails',(req,res)=>{
+     
+    let productid = req.body.productid
+    let quantity = req.body.quantity
+    let mrp = req.body.totalprice
+
+    const  userdetails = new userdetails({
+       _id: productid,
+       quantity:quantity,
+       mrp:mrp,
+       totalprice:totalprice,
+    })
+   
+    if(order_ID=!0){
+        res.send({
+            msg:'These are your order details'+productid,quantity,mrp,totalprice
+            
+        })
+    }
+    else{
+        res.send({
+            msg:'Your order_ID is not generated,get your order_ID to display the cart'
+        })
+    }
+   
+        
+    
+})
+
+
+
+
+router.post('/create/:order_ID',  async (req, res) => {
 
     let productid = req.params.productid
     let quantity = parseInt(req.query.quantity)
@@ -49,11 +84,8 @@ router.post('/update/:order_ID',  async (req, res) => {
 
   
     
-    jwt.verify(req, 'illenium', async (err, authData) => {
-        if(err) {
-            res.sendStatus(403)
-        } else {
-            let result = await updateCart(authData['user']['email'] ,quantity,mrp ,totalprice,productid)
+    
+            let result = await createbill(authData['user']['email'] ,quantity,mrp ,totalprice,productid)
 
             if(result > 0) {
                 res.json({
@@ -70,10 +102,8 @@ router.post('/update/:order_ID',  async (req, res) => {
                 })
             }
 
-        }
-    })
-})
-
+}
+)
 
 
 
@@ -81,8 +111,8 @@ router.post('/update/:order_ID',  async (req, res) => {
 //database
 
 
-async function updateCart(email, data, inc) {
-    const client = await connectDB()
+async function createCart(email, data, inc) {
+     const client = await connectDB()
 
     const available = await (await client.db('illenium').collection('inventory').find({ _id: data},{ projection:{_id:0, quantity:1,mrp:int1,totalprice:int2} }).toArray())[0]['quantity']
 
@@ -95,7 +125,7 @@ async function updateCart(email, data, inc) {
     console.log(inc)
 
     if(quantity + inc <= available) {
-        const result = await (await client.db('illenium').collection('users').updateOne({ cart: { $elemMatch: {product: data, mrp:int1,totalprice:int2}}}, {$inc: { 'cart.$.quantity': inc,} }).catch(Error)).modifiedCount
+        const result = await (await client.db('illenium').collection('users').createOne({ cart: { $elemMatch: {product: data, mrp:int1,totalprice:int2}}}, {$inc: { 'cart.$.quantity': inc,} }).catch(Error)).modifiedCount
         console.log(result)
         return result
     } else {
@@ -105,12 +135,16 @@ async function updateCart(email, data, inc) {
 }
 
 
-async function getCart(email, data){
+async function getbill(email, data){
     const client = await connectDB()
     const result = await client.db('illenium').collection('users').find({ email:email, cart: { $elemMatch: {product: data,mrp,totalprice}} }, { projection:{_id:0, 'cart.$' : 1}}).toArray()
     client.close()
     return result
 }
+
+
+
+
  
 async function connectDB() {
     const client = await mongodb.MongoClient.connect(connection_string, {
